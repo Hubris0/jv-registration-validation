f0fa71f1f2a70a538bd7fff274503d5fe24084e8
user <pawel.m.janik@gmail.com>
2026-07-24 21:43:01 +0200

    Code formatting

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -1,9 +1,10 @@
 package core.basesyntax.service;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.db.Storage;
